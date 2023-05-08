1afe9b261c310b363e5a34e0cf58d10a04837b78
CucuJr <catinean_andrei@yahoo.com>
2023-05-08 20:40:56 +0300

    2nd commit

--- contact/urls.py
+++ contact/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from contact import views
 
 urlpatterns = [
-    path('', views.ContactTemplateView.as_view(), name="contact")
+    path('contact_page/', views.contact_page, name="contact-page"),
+    path('contacts/', views.contacts, name="contacts"),
+    path('manager_details/', views.manager_details, name="manager-details"),
+    path("secretara_details/", views.secretara_details, name="secretara-details"),
+    path("tehnician_details/", views.tehnician_details, name="tehnician-details"),
+    path("casier_details/", views.casier_details, name="casier-details"),
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,101 @@
+from django.http import HttpResponse
+from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
-class ContactTemplateView(TemplateView):
-    template_name = "contact/contact_page.html"
+def contact_page(request):
+    return HttpResponse("Hello Lumee!!")
 
+def contacts(request):
+    list_contacts = {
+        "contacts":[
+            {
+                "nume": "Catinean",
+                "prenume": "Avram",
+                "functia": "Manager",
+                "email": "catinean.avram@cucu.com",
+                "tel": 40744520260
+            },
+            {
+                "nume": "Catinean",
+                "prenume": "Maria",
+                "functia": "Secretara",
+                "email": "catinean.maria@cucu.com",
+                "tel": 40744520262
+            },
+            {
+                "nume": "Catinean",
+                "prenume": "Andrei",
+                "functia": "Tehnician",
+                "email": "catinean.andrei@cucu.com",
+                "tel": 40744520261
+            },
+            {
+                "nume": "Bog",
+                "prenume": "Cristian",
+                "functia": "Casier",
+                "email": "bug.cristian@cucu.com",
+                "tel": 40744520263
+            },
+        ]
+    }
+
+    return render(request, "contact/contact_page.html", list_contacts)
+
+def manager_details(request):
+    manager = {
+        "manager_details": [
+            {
+                "nume": "Catinean",
+                "prenume": "Avram",
+                "functia": "Manager",
+                "email": "catinean.avram@cucu.com",
+                "tel": 40744520260
+            }
+        ]
+    }
+    return render(request, "contact/manager_details.html", manager)
+
+
+def secretara_details(request):
+    secretara = {
+        "secretara_details": [
+            {
+                "nume": "Catinean",
+                "prenume": "Maria",
+                "functia": "Secretara",
+                "email": "catinean.maria@cucu.com",
+                "tel": 40744520262
+            }
+        ]
+    }
+    return render(request, "contact/secretara_details.html", secretara)
+
+
+def tehnician_details(request):
+    tehnician = {
+        "tehnician_details": [
+            {
+                "nume": "Catinean",
+                "prenume": "Andrei",
+                "functia": "Secretara",
+                "email": "catinean.andrei@cucu.com",
+                "tel": 40744520261
+            }
+        ]
+    }
+    return render(request, "contact/tehnician_details.html", tehnician)
+
+def casier_details(request):
+    casier = {
+        "casier_details": [
+            {
+                "nume": "Bog",
+                "prenume": "Cristian",
+                "functia": "Casier",
+                "email": "bug.cristian@cucu.com",
+                "tel": 40744520263
+            }
+        ]
+    }
+    return render(request, "contact/casier_details.html", casier)
